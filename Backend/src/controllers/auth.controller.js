6b5bdfd4ec529ab7c18c8c92968b54debad1d359
gc-MayankPun <punmayank35@gmail.com>
2026-04-28 13:40:22 +0530

    feat: enhance user registration by including token response and user details in the response

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,8 +2,29 @@ import userModel from "../models/user.model.js";
 import { config } from "../config/config.js";
 import jwt from "jsonwebtoken";
 
-async function sendTokenResponse(user, res) {
-  const token = jwt.sign({ id: user._id }, config.JWT_SECRET);
+async function sendTokenResponse(user, res, message) {
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  res.cookie("token", token);
+  res.status(200).json({
+    message,
+    success: true,
+    user: {
+      id: user._id,
+      role: user.role,
+      email: user.email,
+      contact: user.contact,
+      fullname: user.fullname,
+    },
+  });
 }
 
 export const register = async (req, res) => {
@@ -20,6 +41,7 @@ export const register = async (req, res) => {
     }
 
     const user = await userModel.create({ fullname, email, password, contact });
+    await sendTokenResponse(user, res, "User registered successfully");
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Server error" });
